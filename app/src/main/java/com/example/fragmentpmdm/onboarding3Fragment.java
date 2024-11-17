9c2f455a678f3621936078f56a37147f0951f7de
Diego Reynoso <diegoreynoso746@gmail.com>
2024-11-17 18:13:58 +0100

    Modificado

---
+++ app/src/main/java/com/example/fragmentpmdm/onboarding3Fragment.java
@@ -0,0 +1,42 @@
+package com.example.fragmentpmdm;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class onboarding3Fragment extends Fragment {
+
+    Button botonFinish;
+    NavController navController;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_onboarding3, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        botonFinish = view.findViewById(R.id.botonFinish);
+        botonFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_onboarding3Fragment_to_homeFragment);
+            }
+        });
+    }
+}
